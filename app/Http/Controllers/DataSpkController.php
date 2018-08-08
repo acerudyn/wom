9f3d3e59458cbc7bbd2920b02db802dff792e7b6
acerudyn <acerudyn@yahoo.com>
2018-08-08 21:49:10 +0700

    edit filter by title

--- app/Http/Controllers/DataSpkController.php
+++ app/Http/Controllers/DataSpkController.php
@@ -30,6 +30,36 @@ class DataSpkController extends Controller
                                        ->with(compact('partners'));
     }
 
+  public function lookupRo()
+    {
+        //$datas    = Spk::get();
+        $datas    = DB::table('spk')->limit(0)->get();
+        $partners = DB::table('partner')->get();
+
+        return view('admin/dataLookupRo')->with(compact('datas'))
+                                       ->with(compact('partners'));
+    }
+
+  public function lookupMso()
+    {
+        //$datas    = Spk::get();
+        $datas    = DB::table('spk')->limit(0)->get();
+        $partners = DB::table('partner')->get();
+
+        return view('admin/dataLookupMso')->with(compact('datas'))
+                                       ->with(compact('partners'));
+    }
+
+  public function lookupPartner()
+    {
+        //$datas    = Spk::get();
+        $datas    = DB::table('spk')->limit(0)->get();
+        $partners = DB::table('partner')->get();
+
+        return view('admin/dataLookupPartner')->with(compact('datas'))
+                                       ->with(compact('partners'));
+    }
+
   public function filter(Request $request)
     {
 
@@ -50,6 +80,71 @@ class DataSpkController extends Controller
 
         return view('admin/dataLookup')->with(compact('datas'))
                                        ->with(compact('partners'));
+    }
+
+  public function filterRo(Request $request)
+    {
+
+        $partners = DB::table('partner')->get();
+
+        $this->validate($request, [
+            'value_param' => 'required|min:3',
+        ]);
+
+        $id_partner   = $request->id_partner;
+        $parameter    = $request->parameter;
+        $value_param  = $request->value_param;
+
+        $datas = DB::table('spk')->where([
+                                            ['id_partner', $id_partner],
+                                            [$parameter, $value_param],
+                                          ])->get();
+
+        return view('admin/dataLookupRo')->with(compact('datas'))
+                                       ->with(compact('partners'));
+    }
+
+  public function filterMso(Request $request)
+    {
+
+        $partners = DB::table('partner')->get();
+
+        $this->validate($request, [
+            'value_param' => 'required|min:3',
+        ]);
+
+        $id_partner   = $request->id_partner;
+        $parameter    = $request->parameter;
+        $value_param  = $request->value_param;
+
+        $datas = DB::table('spk')->where([
+                                            ['id_partner', $id_partner],
+                                            [$parameter, $value_param],
+                                          ])->get();
+
+        return view('admin/dataLookupMso')->with(compact('datas'))
+                                       ->with(compact('partners'));
+    }
+
+  public function filterPartner(Request $request)
+    {
+
+        $partners = DB::table('partner')->get();
+
+        $this->validate($request, [
+            'value_param' => 'required|min:3',
+        ]);
+
+        $id_partner   = $request->id_partner;
+        $parameter    = $request->parameter;
+        $value_param  = $request->value_param;
+
+        $datas = DB::table('spk')->where([
+                                            ['id_partner', $id_partner],
+                                            [$parameter, $value_param],
+                                          ])->get();
+        return view('admin/dataLookupPartner')->with(compact('datas'))
+                                             ->with(compact('partners'));
     }
 
 
