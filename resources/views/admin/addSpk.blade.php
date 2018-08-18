42041085737c499bbd5cece0e3dcd39b95b7fcbd
acerudyn <acerudyn@yahoo.com>
2018-08-18 10:39:02 +0700

    update balde add spk manual

--- resources/views/admin/addSpk.blade.php
+++ resources/views/admin/addSpk.blade.php
@@ -18,6 +18,141 @@
         <div class="col-md-12">
           <div class="box box-info">
             <div class="box-header with-border">
+              <h3 class="box-title">Form Input SPK</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal" action="{{'uploadSpk'}}" enctype="multipart/form-data" method="post">
+              {{ csrf_field() }}
+              <div class="box-body">
+                <div class="form-group">
+                <label label for="inputEmail3" class="col-sm-2 control-label">Pilih Partner</label>
+                <div class="col-sm-9">
+                  <select class="form-control" name="id_partner" required>
+                    <option value="">Pilih Partner</option>
+                    @foreach($partners as $data)
+                      <option value="{{$data->id_partner}}">{{$data->nama_partner}}</option>
+                    @endforeach
+                  </select>
+               </div>
+              </div>
+			  
+			  <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Tgl SPK</label>
+
+                  <div class="col-sm-9">
+                    <div class="input-group date">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+
+                    <!--
+                      <input class="form-control pull-right" data-inputmask="'alias': 'dd/mm/yyyy'"
+                      data-mask="" type="text" id="tgl_duedate_spk" name="tgl_duedate_spk">
+                    -->
+                      <input type="text" class="form-control pull-right" id="tgl_spk"
+                      name="tgl_spk" required>
+                    </div>
+                  </div>
+                </div>
+				
+                 <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Nama Merchant</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3"
+                     placeholder="Nama Merchant" name="nama_merchant" value="">
+                  </div>
+                </div>
+				
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Alamat Merchant</label>
+
+                  <div class="col-sm-9">
+                    <textarea name="alamat_merchant" rows="5" class="form-control"></textarea>
+                  </div>
+                </div>
+				
+				<div class="form-group">
+					<label label for="inputEmail3" class="col-sm-2 control-label">Kota Merchant</label>
+					<div class="col-sm-9">
+					<input type="text" class="form-control" id="inputEmail3"
+					placeholder="Kota Merchant" name="kota_mso" value="">
+					<input type="hidden" name="nama_ro" value="">
+					</div>
+				</div>
+											
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">TID</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3" value=""
+                     placeholder="TID" name="tid" >
+                  </div>
+                </div>
+				
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">MID</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3" value=""
+                     placeholder="MID" name="mid" >
+                  </div>
+                </div>
+				
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label ">PIC Merchant</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3"
+                     placeholder="PIC" name="pic_merchant" value="">
+                  </div>
+                </div>
+				
+				 <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Kontak</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3"
+                     placeholder="Kontak" name="kontak_merchant" value="">
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Perintah SPK</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3"
+                     placeholder="Perintah Spk" name="perintah_spk" required value="">
+                  </div>
+                </div>
+				
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">SN EDC</label>
+
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="inputEmail3"
+                     placeholder="SN EDC" name="sn_edc"  value="">
+                  </div>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-info pull-right">Submit</button>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+      <!-- /.row -->
+	  
+	  
+	  
+	    <div class="row">
+        <div class="col-md-12">
+          <div class="box box-info">
+            <div class="box-header with-border">
               <h3 class="box-title">Form Input SPK by Excel</h3>
             </div>
             <!-- /.box-header -->
@@ -58,6 +193,8 @@
         </div>
       </div>
       <!-- /.row -->
+	 
+	  
     </section>
     <!-- /.content -->
 @endsection
